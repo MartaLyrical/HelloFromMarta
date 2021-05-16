d34791daf305572fddc1a923c1ca160d756614be
Jonathan Henson <jonathan@henson.me.uk>
2021-05-16 16:41:08 +0100

    Updated main to print a response to the first print message

--- src/Alves/Main.java
+++ src/Alves/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello my name is Marta Alves");
+        System.out.println("Hello Marta, my name is Jonathan.");
     }
 }
